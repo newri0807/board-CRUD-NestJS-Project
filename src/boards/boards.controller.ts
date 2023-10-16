b6477ef298e096f927b666961e86986d36e20049
newri <newri0807@naver.com>
2023-10-16 14:47:20 +0900

    ADD crud

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,11 +1,52 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private BoardsService: BoardsService) {}
 
-  getAllTask() {
-    this.BoardsService;
+  @Get('/')
+  getAllBoard(): Board[] {
+    return this.BoardsService.getAllBoard();
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.BoardsService.createBoard(createBoardDto);
+  }
+
+  // 파라미터 여러개 일때 가져오기
+  // http://localhost:3000/?id=123&text=ttt
+  // (@Param() params: string[])
+
+  // 파라미터 1개 일때 가져오기
+  //http://localhost:3000?id=123
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.BoardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.BoardsService.deleteBoard(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ) {
+    return this.BoardsService.updateBoardStatus(id, status);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards: Board[] = [];
+
+  getAllBoard(): Board[] {
+    return this.boards; // 리턴값 설정
+  }
+
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
+    const board: Board = {
+      id: uuid(), // for unique id
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    this.boards.push(board);
+    return board;
+  }
+
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id != id);
+  }
+
+  // 상태값만 업데이트
+  updateBoardStatus(id: string, status: BoardStatus): Board {
+    const board = this.getBoardById(id);
+    board.status = status;
+    return board;
+  }
+}
