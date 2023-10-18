83a4c27f4e29d083448e3df78bb45d06f265617c
newri <newri0807@naver.com>
2023-10-18 14:57:16 +0900

    ADD Loigin & SingnUp Func2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,10 +14,13 @@ export class AuthService {
 
   async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
     const { username, password } = authCredentialsDto;
+
+    // 1. UserRepository를 통해 사용자 이름이 username과 일치하는 사용자의 데이터를 데이터베이스에서 찾는다.
     const user = await this.UserRepository.findOne({
       where: { username: username },
     });
 
+    //                                  입력값,    db값
     if (user && (await bcrypt.compare(password, user.password))) {
       return 'login success';
     } else {
